2a10ee662fb7f2377df7d8f8ed0e93fc1865c49f
HCI UW <wisconsinhci@gmail.com>
2019-02-17 23:02:16 -0600

    working on gripper control

--- iter_app/src/primitives.py
+++ iter_app/src/primitives.py
@@ -22,8 +22,6 @@ from std_msgs.msg import Header
 from abc import ABCMeta, abstractmethod
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
 
-rospy.sleep(10)
-
 ARM_MOVE_GROUP = rospy.get_param("arm_move_group")
 GRIPPER_MOVE_GROUP = rospy.get_param("gripper_move_group")
 GRIPPER_TYPE = rospy.get_param("gripper_type")
