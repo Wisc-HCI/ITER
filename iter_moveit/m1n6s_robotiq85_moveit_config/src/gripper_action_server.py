92058bb83ab36099aa3933cdaa9c08292d0e6881
HCI UW <wisconsinhci@gmail.com>
2019-02-24 22:55:49 -0600

    robotiq gripper joint feedback to movegroup

--- iter_moveit/m1n6s_robotiq85_moveit_config/src/gripper_action_server.py
+++ iter_moveit/m1n6s_robotiq85_moveit_config/src/gripper_action_server.py
@@ -1,17 +1,22 @@
+#!/usr/bin/env python
+import roslib
+roslib.load_manifest('m1n6s_robotiq85_moveit_config')
 import rospy
 import actionlib
+
 from robotiq_85_msgs.msg import GripperCmd, GripperStat
-from control_msgs.action import FollowJointTrajectoryAction, FollowJointTrajectoryFeedback, FollowJointTrajectoryResult
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryFeedback, FollowJointTrajectoryResult
 
 class GripperActionServer:
 
-    def __init__(self, name):
-        self._action_server = actionlib.SimpleActionServer('/gripper/follow_joint_trajectory', FollowJointTrajectoryAction, execute_cb=self._execute, False)
+    def __init__(self):
+        self._action_server = actionlib.SimpleActionServer('/gripper/follow_joint_trajectory', FollowJointTrajectoryAction, self._execute, False)
         self._action_server.start()
 
     def _execute(self, goal):
         print '\n\n\n\n', goal, '\n\n\n\n'
-        
+        self._action_server.set_succeeded()
+
 
 if __name__ == "__main__":
     rospy.init_node('gripper_action_server')
