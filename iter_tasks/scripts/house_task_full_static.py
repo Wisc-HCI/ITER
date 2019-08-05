7071a0ab95bad330b6fbb4a34d8afb1adba8335f
Curt Henrichs <cdhenrichs@wisc.edu>
2019-08-05 01:26:39 -0500

    static blocks

--- iter_tasks/scripts/house_task_full_static.py
+++ iter_tasks/scripts/house_task_full_static.py
@@ -7,14 +7,13 @@ import copy
 import iter_tasks_tools.data_types as dt
 import iter_tasks_tools.primitives as pm
 
-
-task_case = "pooled"
+task_case = "pooled_static"
 build_house_1 = True
 house_1_allocation = {"robot_base":True,"robot_mid_1":True,"robot_mid_2":True,"robot_roof":True}
 build_house_2 = False
 house_2_allocation = {"robot_base":False,"robot_mid_1":False,"robot_mid_2":False,"robot_roof":False}
 
-SAFE_HEIGHT_OFFSET = 0.1
+AFE_HEIGHT_OFFSET = 0.1
 
 BLOCK_SMALL = (0.02032,0.05842,0.01524)
 BLOCK_LARGE = (0.02032,0.10,0.01524)
@@ -29,21 +28,40 @@ WAIT_POSITION = dt.position(0,0.25,0.3)
 WAIT_ORIENTATION = dt.orientation(0,0,0,1)
 
 WORKSPACE_POSITION = dt.position(0.2,0.25,-0.02)
-REGION_POSITION = dt.position(-0.2,0.45,0.25)
-REGION_ORIENTATION = copy.deepcopy(DOWN_GX_ORIENTATION)
 
 GRASP_EFFORT = 0.59 #0.57
 RELEASE_EFFORT_REGION = 0.25
 RELEASE_EFFORT_WORKSPACE = 0.45
 
-REGION_GRASP_OFFSET_LARGE = dt.pose(dt.position(0.005, 0.005, 0.135),
-                                copy.deepcopy(REGION_ORIENTATION))
-
-REGION_GRASP_OFFSET_SMALL = dt.pose(dt.position(0.005, 0.005, 0.135),
-                                copy.deepcopy(REGION_ORIENTATION))
+REGION_GRASP_OFFSET = dt.pose(dt.position(0.005, 0.005, 0.135),
+                                copy.deepcopy(DOWN_GX_ORIENTATION))
 
 WORKSPACE_GRASP_OFFSET = dt.pose(dt.position(0,0,0.1675),
-                                copy.deepcopy(REGION_ORIENTATION))
+                                copy.deepcopy(DOWN_GX_ORIENTATION))
+
+obj_count = 0
+
+
+class Queue:
+
+    def __init__(self, start_pose, end_pose, num_items):
+        self._start_pose = start_pose
+        self._end_pose = end_pose
+        self._num_items = num_items
+        self._count = 0
+
+    def get(self):
+        dx = self._end_pose['position']['x'] - self._start_pose['position']['x']
+        dy = self._end_pose['position']['y'] - self._start_pose['position']['y']
+        dz = self._end_pose['position']['z'] - self._start_pose['position']['z']
+
+        x = dx * self._count + self._start_pose['position']['x']
+        y = dy * self._count + self._start_pose['position']['y']
+        z = dz * self._count + self._start_pose['position']['z']
+
+        self._count += 1 / self._num_items
+
+        return dt.pose(dt.position(),copy.deepcopy(DOWN_GX_ORIENTATION))
 
 
 def move_home():
@@ -58,26 +76,34 @@ def wait_for_human():
     task_list.append(pm.wait_button())
     return task_list
 
-def pick_and_place_block(block_type,target_position,target_orientation,grasp_offset,vision_params):
+def pick_and_place_block(queue, target_position,target_orientation):
+    global obj_count
+
     task_list = []
 
-    safe_position = copy.deepcopy(target_position)
-    safe_position['z'] += SAFE_HEIGHT_OFFSET
+    safe_target_position = copy.deepcopy(target_position)
+    safe_target_position['z'] += SAFE_HEIGHT_OFFSET
 
-    task_list.append(pm.pick_and_place_vision(
-        object_type=block_type,
-        path_to_region=[],
+    object_pose = queue.get()
+    safe_object_position = copy.deepcopy(object_pose['position'])
+    safe_object_position['z'] += SAFE_HEIGHT_OFFSET
+
+    task_list.append(pm.pick_and_place_static(
+        path_to_object=[
+            pm.move(safe_object_position,object_pose['orientation']),
+            pm.move(object_pose['position'],object_pose['orientation'])
+        ],
         path_to_destination=[
-            pm.move(safe_position,target_orientation),
+            pm.move(safe_target_position,target_orientation),
             pm.move(target_position,target_orientation)
         ],
+        object_name=obj_count,
         grasp_effort=GRASP_EFFORT,
-        release_effort=RELEASE_EFFORT_WORKSPACE,
-        grasp_offset=grasp_offset,
-        safe_height=SAFE_HEIGHT_OFFSET,
-        vision_params=vision_params))
+        release_effort=RELEASE_EFFORT_WORKSPACE))
 
-    task_list.append(pm.move(safe_position,target_orientation))
+    obj_count += 1
+
+    task_list.append(pm.move(safe_target_position,target_orientation))
     task_list.append(pm.release(RELEASE_EFFORT_REGION))
     return task_list
 
@@ -93,28 +119,18 @@ def build_house(workplace_position, robot_base=True, robot_mid_1=True, robot_mid
         position['y'] += 0.5 * BLOCK_LARGE[1] + WORKSPACE_GRASP_OFFSET['position']['y']
         position['z'] += 0.5 * BLOCK_LARGE[2] + WORKSPACE_GRASP_OFFSET['position']['z'] + 0.0005
         task_list += pick_and_place_block(
-            block_type='large',
             target_position=copy.deepcopy(position),
             target_orientation=DOWN_GY_ORIENTATION,
-            grasp_offset=REGION_GRASP_OFFSET_LARGE,
-            vision_params={
-                            "min_hue": 0,
-                            "max_hue": 180
-                          })
+            grasp_offset=REGION_GRASP_OFFSET)
 
         position = copy.deepcopy(workplace_position)
         position['x'] += BLOCK_SMALL[1] - 0.5 * BLOCK_LARGE[0] + WORKSPACE_GRASP_OFFSET['position']['x']
         position['y'] += 0.5 * BLOCK_LARGE[1] + WORKSPACE_GRASP_OFFSET['position']['y']
         position['z'] += 0.5 * BLOCK_LARGE[2] + WORKSPACE_GRASP_OFFSET['position']['z'] + 0.0005
         task_list += pick_and_place_block(
-            block_type='large',
             target_position=copy.deepcopy(position),
             target_orientation=DOWN_GY_ORIENTATION,
-            grasp_offset=REGION_GRASP_OFFSET_LARGE,
-            vision_params={
-                            "min_hue": 0,
-                            "max_hue": 180
-                          })
+            grasp_offset=REGION_GRASP_OFFSET)
     else:
         task_list += wait_for_human()
 
@@ -124,28 +140,18 @@ def build_house(workplace_position, robot_base=True, robot_mid_1=True, robot_mid
         position['y'] += 0.5 * BLOCK_LARGE[0] + WORKSPACE_GRASP_OFFSET['position']['y']
         position['z'] += 1.5 * BLOCK_SMALL[2] + WORKSPACE_GRASP_OFFSET['position']['z'] + 0.0005
         task_list += pick_and_place_block(
-            block_type='small',
             target_position=copy.deepcopy(position),
             target_orientation=DOWN_GX_ORIENTATION,
-            grasp_offset=REGION_GRASP_OFFSET_SMALL,
-            vision_params={
-                            "min_hue": 0,
-                            "max_hue": 180
-                          })
+            grasp_offset=REGION_GRASP_OFFSET)
 
         position = copy.deepcopy(workplace_position)
         position['x'] += BLOCK_SMALL[0] + WORKSPACE_GRASP_OFFSET['position']['x']
         position['y'] += BLOCK_LARGE[1] - 0.5 * BLOCK_SMALL[0] + WORKSPACE_GRASP_OFFSET['position']['y']
         position['z'] += 1.5 * BLOCK_SMALL[2] + WORKSPACE_GRASP_OFFSET['position']['z'] + 0.0005
         task_list += pick_and_place_block(
-            block_type='small',
             target_position=copy.deepcopy(position),
             target_orientation=DOWN_GX_ORIENTATION,
-            grasp_offset=REGION_GRASP_OFFSET_SMALL,
-            vision_params={
-                            "min_hue": 0,
-                            "max_hue": 180
-                          })
+            grasp_offset=REGION_GRASP_OFFSET)
     else:
         task_list += wait_for_human()
 
@@ -155,28 +161,18 @@ def build_house(workplace_position, robot_base=True, robot_mid_1=True, robot_mid
         position['y'] += 0.5 * BLOCK_LARGE[1] + WORKSPACE_GRASP_OFFSET['position']['y']
         position['z'] += 2.5 * BLOCK_LARGE[2] + WORKSPACE_GRASP_OFFSET['position']['z'] + 0.0005
         task_list += pick_and_place_block(
-            block_type='large',
             target_position=copy.deepcopy(position),
             target_orientation=DOWN_GY_ORIENTATION,
-            grasp_offset=REGION_GRASP_OFFSET_LARGE,
-            vision_params={
-                            "min_hue": 0,
-                            "max_hue": 180
-                          })
+            grasp_offset=REGION_GRASP_OFFSET)
 
         position = copy.deepcopy(workplace_position)
         position['x'] += BLOCK_SMALL[1] - 0.5 * BLOCK_LARGE[0] + WORKSPACE_GRASP_OFFSET['position']['x']
         position['y'] += 0.5 * BLOCK_LARGE[1] + WORKSPACE_GRASP_OFFSET['position']['y']
         position['z'] += 2.5 * BLOCK_LARGE[2] + WORKSPACE_GRASP_OFFSET['position']['z'] + 0.0005
         task_list += pick_and_place_block(
-            block_type='large',
             target_position=copy.deepcopy(position),
             target_orientation=DOWN_GY_ORIENTATION,
-            grasp_offset=REGION_GRASP_OFFSET_LARGE,
-            vision_params={
-                            "min_hue": 0,
-                            "max_hue": 180
-                          })
+            grasp_offset=REGION_GRASP_OFFSET)
     else:
         task_list += wait_for_human()
 
@@ -187,14 +183,9 @@ def build_house(workplace_position, robot_base=True, robot_mid_1=True, robot_mid
         position['y'] += 0.5 * BLOCK_LARGE[1] + 0.1 * BLOCK_SMALL[0] + WORKSPACE_GRASP_OFFSET['position']['y']
         position['z'] += 3.5 * BLOCK_SMALL[2] + WORKSPACE_GRASP_OFFSET['position']['z'] + 0.0005
         task_list += pick_and_place_block(
-            block_type='small',
             target_position=copy.deepcopy(position),
             target_orientation=DOWN_GX_ORIENTATION,
-            grasp_offset=REGION_GRASP_OFFSET_SMALL,
-            vision_params={
-                            "min_hue": 0,
-                            "max_hue": 180
-                          })
+            grasp_offset=REGION_GRASP_OFFSET)
 
         # angles
         position = copy.deepcopy(workplace_position)
@@ -202,56 +193,36 @@ def build_house(workplace_position, robot_base=True, robot_mid_1=True, robot_mid
         position['y'] += 0.2 * BLOCK_LARGE[1] + WORKSPACE_GRASP_OFFSET['position']['y']
         position['z'] += 4 * BLOCK_LARGE[2] + WORKSPACE_GRASP_OFFSET['position']['z'] + 0.0005
         task_list += pick_and_place_block(
-            block_type='small',
             target_position=copy.deepcopy(position),
             target_orientation=DOWN_GY_ORIENTATION,
-            grasp_offset=REGION_GRASP_OFFSET_SMALL,
-            vision_params={
-                            "min_hue": 0,
-                            "max_hue": 180
-                          })
+            grasp_offset=REGION_GRASP_OFFSET)
 
         position = copy.deepcopy(workplace_position)
         position['x'] += BLOCK_SMALL[1] - 0.5 * BLOCK_LARGE[0] + WORKSPACE_GRASP_OFFSET['position']['x']
         position['y'] += 0.2 * BLOCK_LARGE[1] + WORKSPACE_GRASP_OFFSET['position']['y']
         position['z'] += 4 * BLOCK_LARGE[2] + WORKSPACE_GRASP_OFFSET['position']['z'] + 0.0005
         task_list += pick_and_place_block(
-            block_type='small',
             target_position=copy.deepcopy(position),
             target_orientation=DOWN_GY_ORIENTATION,
-            grasp_offset=REGION_GRASP_OFFSET_SMALL,
-            vision_params={
-                            "min_hue": 0,
-                            "max_hue": 180
-                          })
+            grasp_offset=REGION_GRASP_OFFSET)
 
         position = copy.deepcopy(workplace_position)
         position['x'] += 0.5 * BLOCK_LARGE[0] + WORKSPACE_GRASP_OFFSET['position']['x']
         position['y'] += 0.8 * BLOCK_LARGE[1] + WORKSPACE_GRASP_OFFSET['position']['y']
         position['z'] += 4 * BLOCK_LARGE[2] + WORKSPACE_GRASP_OFFSET['position']['z'] + 0.0005
         task_list += pick_and_place_block(
-            block_type='small',
             target_position=copy.deepcopy(position),
             target_orientation=DOWN_GY_ORIENTATION,
-            grasp_offset=REGION_GRASP_OFFSET_SMALL,
-            vision_params={
-                            "min_hue": 0,
-                            "max_hue": 180
-                          })
+            grasp_offset=REGION_GRASP_OFFSET)
 
         position = copy.deepcopy(workplace_position)
         position['x'] += BLOCK_SMALL[1] - 0.5 * BLOCK_LARGE[0] + WORKSPACE_GRASP_OFFSET['position']['x']
         position['y'] += 0.8 * BLOCK_LARGE[1] + WORKSPACE_GRASP_OFFSET['position']['y']
         position['z'] += 4 * BLOCK_LARGE[2] + WORKSPACE_GRASP_OFFSET['position']['z'] + 0.0005
         task_list += pick_and_place_block(
-            block_type='small',
             target_position=copy.deepcopy(position),
             target_orientation=DOWN_GY_ORIENTATION,
-            grasp_offset=REGION_GRASP_OFFSET_SMALL,
-            vision_params={
-                            "min_hue": 0,
-                            "max_hue": 180
-                          })
+            grasp_offset=REGION_GRASP_OFFSET)
 
     else:
         task_list += wait_for_human()
@@ -291,7 +262,7 @@ if __name__ == "__main__":
     env_list += static_environment()
 
     plan = dt.plan(
-        title='UR3e House Full Task',
+        title='UR3e House Full Task - Static',
         author='Curt Henrichs',
         description='Build house task',
         version='0.0.1',
