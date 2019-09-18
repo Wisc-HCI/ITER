57c16d15164771279def5ca1105862e2ea706962
HCI UW <wisconsinhci@gmail.com>
2019-09-18 17:15:37 -0500

    using both real and fake buttons

--- iter_app.sh
+++ iter_app.sh
@@ -4,4 +4,4 @@ cd ~/Workspaces/iter_ws
 pwd
 source ./devel/setup.bash
 
- exec roslaunch iter_app main.launch robot:=ur3e planner:=ur simulated:=false use_fake_btn:=false rad_ui:=true
+ exec roslaunch iter_app main.launch robot:=ur3e planner:=ur simulated:=false use_fake_btn:=true use_real_btn:=true rad_ui:=true
